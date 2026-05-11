751057e5a18d1f90ff512a0ad943d3a26550776f
WoonKang <e2505011@gmail.com>
2026-05-12 00:42:26 +0900

    mountain_top

--- src/main/java/algorithm_string/extract_numbers.java
+++ src/main/java/algorithm_string/extract_numbers.java
@@ -1,4 +1,23 @@
 package algorithm_string;
 
+import java.util.*;
+
 public class extract_numbers {
+    public static void main (String[] argv) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        StringBuilder result = new StringBuilder("");
+        int i = 0;
+        while (i < str.length()) {
+            if (str.charAt(i) > '0' && str.charAt(i) <= '9')
+                break;
+            i ++;
+        }
+        while (i < str.length()) {
+            if (str.charAt(i) >= '0' && str.charAt(i) <= '9')
+                result.append(str.charAt(i));
+            i ++;
+        }
+        System.out.println(result.toString());
+    }
 }
---
+++ src/main/java/alogorithm_array/mountain_top.java
@@ -0,0 +1,61 @@
+package alogorithm_array;
+
+import java.util.*;
+
+public class mountain_top {
+    private static boolean count_neighbor (List<Integer>[] arr, int i, int j, int N) {
+        int count = 0;
+        if (i > 0) {
+            if (arr[i - 1].get(j) < arr[i].get(j))
+                count++;
+        } else {
+            count++;
+        }
+        if (i < N - 1) {
+            if (arr[i + 1].get(j) < arr[i].get(j))
+                count++;
+        } else {
+            count++;
+        }
+        if (j > 0) {
+            if (arr[i].get(j - 1) < arr[i].get(j))
+                count++;
+        }  else {
+            count++;
+        }
+        if (j < N - 1) {
+            if (arr[i].get(j + 1) < arr[i].get(j))
+                count++;
+        } else {
+            count++;
+        }
+        if (count == 4)
+            return (true);
+        else
+            return (false);
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int mount_top_number = 0;
+        boolean it_top = false;
+
+        int N = Integer.parseInt(kb.nextLine());
+        List<Integer>[] arr = new ArrayList[N];
+        for (int i = 0; i < N; i ++) {
+            arr[i] = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
+                arr[i].add(Integer.valueOf(kb.next()));
+            }
+        }
+
+        for (int i = 0; i < N; i ++) {
+            for (int j = 0; j < N; j++) {
+                it_top = count_neighbor(arr, i, j, N);
+                if (it_top)
+                    mount_top_number ++;
+            }
+        }
+        System.out.println(mount_top_number);
+    }
+}
