35a74ade4515d84b3950e0378ab64b08e74de9af
woonkim0413 <dnsrkd0414@naver.com>
2026-08-26 12:10:46 +0900

    [algorithm study][programmers] 단어 변환 level 3

--- src/main/java/programmers/network_dfs_leve3.java
+++ src/main/java/programmers/network_dfs_leve3.java
@@ -1,5 +1,36 @@
 package programmers;
 
-public class network_dfs_leve3 {
+import java.util.*;
 
+public class network_dfs_leve3 {
+	
+
+	static class Solution {
+	    public int solution(int n, int[][] computers) {
+	        int answer = 0;
+	        Deque<Integer> queue = new ArrayDeque<>();
+	        boolean[] visited = new boolean[n];
+	        
+	        for (int i = 0; i < n; i ++) {
+	            if (!visited[i]) {
+	                answer ++;
+	                
+	                visited[i] = true;
+	                queue.addLast(i);
+	                
+	                while (!queue.isEmpty()) {
+	                    int curNode = queue.pollFirst();
+	                    
+	                    for (int j = 0; j < n; j ++) {
+	                        if (!visited[j] && computers[curNode][j] == 1) {
+	                            visited[j] = true;
+	                            queue.addLast(j);
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	        return answer;
+	    }
+	}
 }
---
+++ src/main/java/programmers/transformer_word_level3.java
@@ -0,0 +1,5 @@
+package programmers;
+
+public class transformer_word_level3 {
+
+}
